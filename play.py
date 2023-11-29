ca326d7dd1ce2db44592747006bad2252fe62c14
Shaddy <shadybett540@gmail.com>
2023-11-30 00:26:49 +0300

    done

--- play.py
+++ play.py
@@ -6,8 +6,9 @@ class members:
         return f'Her name is {self.name} and she is {self.age} years old'
     
 m = members(name='sofia', age=21) 
+print(m.name)
+print(m.age)
 # print(m)    
 choice = input('Who is she: ')
 
-setattr(m,choice,'oops')
-print(m)
+print(setattr(m,choice,'oops'))
