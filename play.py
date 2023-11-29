c7ebc70ca782053f077fdade37df1239175b01d5
Shaddy <shadybett540@gmail.com>
2023-11-30 00:56:55 +0300

    done

--- play.py
+++ play.py
@@ -1,18 +1,6 @@
-class members:
-    def __init__(self,name,age):
+class students:
+    def __init__(self,name,age) -> None:
         self.name = name
         self.age = age
-    def __str__(self) -> str:
-        return f'Her name is {self.name} and she is {self.age} years old'
-    
-m = members(name='sofia', age=21) 
-print(m)    
-choice = input('Who is she: ')
 
-print(getattr(m,choice,'oops'))
-
-choice = input('which value do you want to change: ')
-value = input('what do you want it to be: ')
-
-setattr(m,choice,value)
-print(f'Her name is {m.name} and she is {m.age} years old')
+        
