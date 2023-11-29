abae80ef8a4323c04841da036ab78d6f710a1c7a
Shaddy <shadybett540@gmail.com>
2023-11-30 01:05:42 +0300

    done

--- play.py
+++ play.py
@@ -4,9 +4,12 @@ class students:
         self.age = age
     def __str__(self) -> str:
         return(f'He is {self.name} and {self.age} years old')
+    
 
 student1 = students('Anto', 20)
 student2 = students('Bett',18)
 student3 = students('Alfa',19)
 
-print(students)
+print(student2)
+
+choice = input('what do you want: ')
