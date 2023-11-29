3dc3c9c174272ea8f7716cd28a933c46bc78cef4
Shaddy <shadybett540@gmail.com>
2023-11-30 01:02:20 +0300

    name

--- play.py
+++ play.py
@@ -2,5 +2,11 @@ class students:
     def __init__(self,name,age) -> None:
         self.name = name
         self.age = age
+    def __str__(self) -> str:
+        return(f'He is {self.name} and {self.age} years old')
 
-    
+student1 = students('Anto', 20)
+student2 = students('Bett',18)
+student3 = students('Alfa',19)
+
+print(students)
