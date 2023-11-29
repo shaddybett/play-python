5cfc3e00f9e5fb9c0dd05eb81da48ceae43dca82
Shaddy <shadybett540@gmail.com>
2023-11-30 00:14:16 +0300

    done

--- play.py
+++ play.py
@@ -3,6 +3,6 @@ class members:
         self.name = name
         self.age = age
     def __str__(self) -> str:
-        return f''
+        return f'Her name is {self.name}'
 m = members(name='sofia', age=21) 
 print(m)       
