95bb0933671ef5024d8b11961bc1ca18d2aa8198
Shaddy <shadybett540@gmail.com>
2023-11-30 22:00:07 +0300

    done

--- api.py
+++ api.py
@@ -3,3 +3,8 @@ import requests
 response = requests.get('https://randomuser.me/api')
 print(response.json())
 print(response.status_code)
+gender = response.json()['results'][0]['gender']
+title = response.json()['results'][0]['name']['title']
+first_name = response.json()['results'][0]['name']['first']
+last_name = response.json()['results'][0]['name']['last']
+print(f'{title}. {first_name} {last_name} {gender}')
