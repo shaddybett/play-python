1f673cff569cce3ce9d9c5be6a1e3361ec2b564a
Shaddy <shadybett540@gmail.com>
2023-12-05 20:39:44 +0300

    inserted some values

--- shady-bett.session.sql
+++ shady-bett.session.sql
@@ -1,16 +1,47 @@
 CREATE TABLE park(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(250) NOT NULL
-)
+    name VARCHAR(250) NOT NULL,
+    location REFERENCES location (id)
+    animals REFERENCES animals (id)
+);
 CREATE TABLE location(
     id SERIAL PRIMARY KEY,
-    code INTEGER
-)
-CREATE TABLE park(
+    code INTEGER,
+);
+CREATE TABLE animals(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(250) NOT NULL
-)
-CREATE TABLE park(
+    name VARCHAR(250) NOT NULL,
+    top_animal REFERENCES lion (id)
+);
+CREATE TABLE lion(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(250) NOT NULL
+    name VARCHAR(250) NOT NULL,
+    cub REFERENCES cub (id)
+);
+CREATE TABLE cub(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    lion REFERENCES lion (id)
 )
+INSERT INTO park(id,name)
+VALUES
+(1,'Kruger'),
+(2,'Tsavo')
+
+INSERT INTO location(id,name)
+VALUES
+(1,'Tanzania'),
+(2,'Kenya')
+
+INSERT INTO animals(id,name)
+VALUES
+(1,'Lion'),
+(2,'Skunk')
+
+INSERT INTO cub(id,name)
+VALUES
+(1,'Shibli'),
+(2,'bob')
+
+
+
