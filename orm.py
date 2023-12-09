1ac94a359aeef4fcd6863c5ab64f6a4c9dbf3615
Shaddy <shadybett540@gmail.com>
2023-12-09 21:19:01 +0300

    done

--- orm.py
+++ orm.py
@@ -3,5 +3,30 @@ CONN = sqlite3.connect('db/ormpets.db')
 CURSOR = CONN.cursor
 
 class Dog:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,name,breed):
+        self.id = None
+        self.name = name
+        self.breed = breed
+
+    @classmethod
+    def create_table(self):
+
+        sql = """
+        CREATE TABLE IF NOT EXISTS pets(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT        
+        );
+        """
+        CURSOR(sql) 
+        CONN.commit   
+    def insert(self):
+        sql = """
+            INSERT INTO pets(name,breed)
+            VALUES
+            (?,?)
+       """    
+        CURSOR(sql,('name','breed'))
+        CONN.commit
+        
+        
