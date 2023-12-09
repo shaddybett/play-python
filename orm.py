9734d5c6151dcfd37721496972ab4511bd8ff76c
Shaddy <shadybett540@gmail.com>
2023-12-09 20:35:21 +0300

    done

--- orm.py
+++ orm.py
@@ -3,5 +3,8 @@ CONN = sqlite3('db/play.db')
 CURSOR = CONN.CURSOR
 
 class Dog:
+    pets = []
     def __init__(self,name,breed) -> None:
         self.name = name
+        self.breed = breed
+        .add_pets_to_pets(self)
