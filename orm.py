5326bbc8b66eb7e31d16cae7f5b1e4abcf678253
Shaddy <shadybett540@gmail.com>
2023-12-09 21:38:02 +0300

    done

--- orm.py
+++ orm.py
@@ -1,34 +1,53 @@
 import sqlite3
-CONN = sqlite3.connect('db/ormpets.db')
+import os
+
+# Get the absolute path to the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Corrected: sqlite3.connect instead of sqlite3
+CONN = sqlite3.connect('db/play.db')
 CURSOR = CONN.cursor()
 
 class Dog:
-    def __init__(self,name,breed):
+ 
+    def __init__(self, name, breed):
         self.id = None
         self.name = name
         self.breed = breed
 
     @classmethod
     def create_table(cls):
-
+        """
+        CREATE TABLE IF NOT EXISTS pets (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        );
+        """
+        # Corrected: Added the execution of the SQL statement
         sql = """
-            CREATE TABLE IF NOT EXISTS pets(
+            CREATE TABLE IF NOT EXISTS pets (
                 id INTEGER PRIMARY KEY,
                 name TEXT,
-                breed TEXT        
+                breed TEXT
             )
         """
         CURSOR.execute(sql)
         CONN.commit()
-   
+
     def insert(self):
+        """
+        INSERT INTO pets (name, breed)
+        VALUES (?, ?)
+        """
+        # Corrected: Added the execution of the SQL statement
         sql = """
-            INSERT INTO pets(name,breed)
-            VALUES(?,?)
-        """  
-        CURSOR.execute(sql,(self.name,self.breed))
-        CONN.commit()  
+            INSERT INTO pets (name, breed)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.breed))
+        CONN.commit()
 
     def save(self):
-        CONN.commit()    
-        
+        # Corrected: Changed 'CURSOR.save' to 'CONN.commit' to save changes to the database
+        CONN.commit()
