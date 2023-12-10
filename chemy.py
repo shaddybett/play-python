d5b6b6eaf11e47c79425250972e470b7da33000b
Shaddy <shadybett540@gmail.com>
2023-12-10 12:46:55 +0300

    made users table

--- chemy.py
+++ chemy.py
@@ -4,4 +4,13 @@ from sqlalchemy.orm import sessionmaker
 import uuid
 
 Base = declarative_base()
+def generate_uuid():
+    return str(uuid.uuid4())
 
+class users(Base):
+    __tablename__= 'users'
+    userID = Column('userID',String,primary_key=True,default=generate_uuid)
+    firstName = Column('firstName', String)
+    lastName = Column('lastName',String)
+    profileName = Column('profileName',String)
+    email = Column('email',String)
