6e2b3c9e7f8aa4695929b57acd5c1077f644468a
Shaddy <shadybett540@gmail.com>
2023-12-10 12:40:41 +0300

    made base

--- chemy.py
+++ chemy.py
@@ -2,3 +2,5 @@ from sqlalchemy import create_engine,ForeignKey,String,integer,Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import uuid
+
+Base = declarative_base()
