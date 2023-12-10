0aa815c5f9e72463e94b3c57c229c44bd1331b49
Shaddy <shadybett540@gmail.com>
2023-12-10 13:14:26 +0300

    done

--- chemy.py
+++ chemy.py
@@ -7,3 +7,13 @@ Base = declarative_base
 def generate_uuid():
     return str(uuid.uuid4())
 
+class users:
+    __tablename__= 'users'
+    userID = Column('userID',String,primary_key=True,default=generate_uuid)
+    firstName = Column('firstName',String)
+    lastName = Column('lastName',String)
+    email = Column('email',String)
+    profileName = Column('profileName',String)
+
+    def __init__(self) -> None:
+        
