01ba5840c3e2b2ead075f7f89c32ad0badee016e
Shaddy <shadybett540@gmail.com>
2023-12-13 00:37:29 +0300

    added a session

--- chemy-crud.py
+++ chemy-crud.py
@@ -3,8 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-
+Session = sessionmaker
+session = Session()
 class student(Base):
     __tablename__ = 'students'
     studentId = Column(Integer,primary_key=True),
-    
+    studentName = Column(String),
+    studentAge = Column(Integer)
+session.commit(student)
+session.close()    
