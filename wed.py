4d9873c5f36496c28bc4ac4594aac84ad4c36de1
Shaddy <shadybett540@gmail.com>
2023-12-13 21:16:08 +0300

    fixed error

--- wed.py
+++ wed.py
@@ -6,7 +6,7 @@ import uuid
 def generate_uuid():
     return str(uuid.uuid4())
 
-Base = declarative_base()  # Corrected this line
+Base = declarative_base()  
 db = 'sqlite:///wedDB.db'
 engine = create_engine(db)
 Base.metadata.create_all(bind=engine)
@@ -14,26 +14,23 @@ Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-class Book:
+class Book(Base):  # Inherit from Base
     __tablename__='books'
-    bookId = Column('bookId',String,primary_key=True,default=generate_uuid)
-    bookName = Column('bookName',String)
-    bookPages = Column('bookPages',Integer)
-    bookAuthor = Column('bookAuthor',String)
+    bookId = Column('bookId', String, primary_key=True, default=generate_uuid)
+    bookName = Column('bookName', String)
+    bookPages = Column('bookPages', Integer)
+    bookAuthor = Column('bookAuthor', String)
 
-    def __init__(self,bookName,bookPages,bookAuthor):
-        self.bookId = None
-        self.bookName = bookName
-        self.bookAuthor = bookAuthor
-        self.bookPages = bookPages
     @classmethod    
-    def add_books(cls,bookName,bookPages,bookAuthor):
+    def add_books(cls, bookName, bookPages, bookAuthor):
         exists = session.query(cls).filter(cls.bookName == bookName).all()
-        if len(exists)>0:
+        if len(exists) > 0:
             print('Book already exists')
         else:
-            new_book = cls(bookAuthor=bookAuthor,bookName=bookName,bookPages=bookPages)
+            new_book = cls(bookAuthor=bookAuthor, bookName=bookName, bookPages=bookPages)
             session.add(new_book)
             session.commit()
             print('New book added')          
-Book.add_books('The Great Gatsby',1200,'F.scott Haddersfield')
+
+# Example usage:
+Book.add_books('The Great Gatsby', 1200, 'F. Scott Haddersfield')
