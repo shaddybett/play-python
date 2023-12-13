c02365bb23284301b130485f6f5a64ed2c2b4110
Shaddy <shadybett540@gmail.com>
2023-12-13 19:55:04 +0300

    completed the starting section

--- wed.py
+++ wed.py
@@ -1,34 +1,15 @@
-# from sqlalchemy import create_engine, Integer, String, Column
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# import uuid
-
-# def generate_uuid():
-#     return str(uuid.uuid4())
-
-# Base = declarative_base()  # Corrected this line
-# db = 'sqlite:///wedDB.db'
-# engine = create_engine(db)
-# Base.metadata.create_all(bind=engine)
-
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-
-from sqlalchemy import create_engine,String,Column,Integer
+from sqlalchemy import create_engine, Integer, String, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import uuid
 
 def generate_uuid():
-    return str (uuid.uuid4())
+    return str(uuid.uuid4())
 
-Base = declarative_base()
-
+Base = declarative_base()  # Corrected this line
 db = 'sqlite:///wedDB.db'
 engine = create_engine(db)
 Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(bind=engine)
-session = Session() 
-
+session = Session()
