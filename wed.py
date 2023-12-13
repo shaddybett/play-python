82b3345ae5383336570023916aed439932dfdb50
Shaddy <shadybett540@gmail.com>
2023-12-13 19:32:53 +0300

    added Base

--- wed.py
+++ wed.py
@@ -2,3 +2,9 @@ from sqlalchemy import create_engine,Integer,String,Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import uuid
+
+def generate_uuid():
+    return str (uuid.uuid4())
+Base = declarative_base
+db = create_engine
+engine = create_engine(db)
