5a06401a41f7a55eb3073e16e6aff0a0bc6319a2
Shaddy <shadybett540@gmail.com>
2023-12-13 14:53:04 +0300

    corrected naming to follow pythonic ways

--- chemy-crud.py
+++ chemy-crud.py
@@ -3,7 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import uuid
 
-generate_id = uuid()
+def generate_uuid():
+     return str(uuid.uuid4())
+
+
 Base = declarative_base()
 db = 'sqlite:///bestDB.db'
 engine = create_engine(db)
@@ -14,10 +17,10 @@ session = Session()
 
 class Student(Base):
     __tablename__='students'
-    studentId = Column(Integer,primary_key=True,default=generate_id)
-    studentName = Column(String)
-    studentEmail = Column(String)
-    studentAge = Column(Integer)
+    studentId = Column('studentId',String,primary_key=True,default=generate_uuid)
+    studentName = Column('studentName',String)
+    studentEmail = Column('studentEmail',String)
+    studentAge = Column('studentAge',Integer)
 
     def __init__(self,studentAge,studentEmail,studentName):
         self.studentId = None
@@ -25,13 +28,13 @@ class Student(Base):
         self.studentEmail = studentEmail
         self.studentName = studentName
 
-def addStudent(studentAge,studentEmail,studentName):
+def add_Student(studentAge,studentEmail,studentName):
         exists = session.query(Student).filter(Student.studentEmail == studentEmail).all()
         if len(exists) > 0:
             print('Email already exists')
         else:
-            newStudent = Student(studentAge,studentEmail,studentName)
-            session.add(newStudent)
-            Session.commit
+            new_Sudent = Student(studentAge,studentEmail,studentName)
+            session.add(new_Sudent)
+            session.commit
             print('Added new student')
-addStudent(20,'mark@gmail.com','Marcos')            
+add_Student(20,'mark@gmail.com','Marcos')            
